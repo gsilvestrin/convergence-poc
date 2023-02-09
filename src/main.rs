30a423d660d7e39d32aa592b1e86e514465ea25d
gsilvestrin <gc@eto.ai>
2023-02-09 14:38:36 -0800

    Sharing state

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
 mod poc;
 
-fn main() {
-    println!("Hello, world!");
+use tokio;
+use convergence::server::{self, BindOptions};
+use std::sync::Arc;
+use std::thread::sleep_ms;
+use convergence::connection::Connection;
+use poc::POCEngine;
+
+async fn start_server() {
+    let duck_conn = Connection::open_in_memory().unwrap();
+
+    let port = server::run_background(
+        BindOptions::new().with_port(0),
+        Arc::new(|| Box::pin(async { POCEngine { duck_conn } })),
+    )
+        .await
+        .unwrap();
+
+    println!("PG Started on port {}", port);
+}
+
+#[tokio::main]
+async fn main() {
+    start_server().await;
+    sleep_ms(60 * 1000);
 }
--- src/poc.rs
+++ src/poc.rs
@@ -1,29 +1,49 @@
 use async_trait::async_trait;
+use duckdb::Connection;
 use convergence::engine::{Engine, Portal};
-use convergence::protocol::{ErrorResponse, FieldDescription};
+use convergence::protocol::{DataTypeOid, ErrorResponse, FieldDescription, SqlState};
 use convergence::protocol_ext::DataRowBatch;
-use sqlparser::ast::{Statement};
+use sqlparser::ast::{Expr, SelectItem, SetExpr, Statement};
 
-struct POCPortal {
+pub struct POCPortal {
     // df: Arc<DataFrame>,
 }
 
 #[async_trait]
 impl Portal for POCPortal {
     async fn fetch(&mut self, batch: &mut DataRowBatch) -> Result<(), ErrorResponse> {
-        Ok(())
-    }
+        let mut row = batch.create_row();
+        row.write_int4(1);
+        Ok(())    }
 }
 
-struct POCEngine;
+pub struct POCEngine {
+    pub duck_conn: Connection
+}
 
 #[async_trait]
 impl Engine for POCEngine {
     type PortalType = POCPortal;
 
-    async fn prepare(&mut self, stmt: &Statement) -> Result<Vec<FieldDescription>, ErrorResponse> {
-        todo!()
-    }
+    async fn prepare(&mut self, statement: &Statement) -> Result<Vec<FieldDescription>, ErrorResponse> {
+        if let Statement::Query(query) = &statement {
+            if let SetExpr::Select(select) = &*query.body {
+                if select.projection.len() == 1 {
+                    if let SelectItem::UnnamedExpr(Expr::Identifier(column_name)) = &select.projection[0] {
+                        match column_name.value.as_str() {
+                            "test_error" => return Err(ErrorResponse::error(SqlState::DATA_EXCEPTION, "test error")),
+                            "test_fatal" => return Err(ErrorResponse::fatal(SqlState::DATA_EXCEPTION, "fatal error")),
+                            _ => (),
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(vec![FieldDescription {
+            name: "test".to_owned(),
+            data_type: DataTypeOid::Int4,
+        }])    }
 
     async fn create_portal(&mut self, _: &sqlparser::ast::Statement) -> Result<Self::PortalType, ErrorResponse> {
         Ok(POCPortal{})
